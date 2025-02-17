2e24ed1d88a87427511ac808d9d0e573d5a5034a
fr4ncisx <fr4ncisx@gmail.com>
2025-02-16 21:36:26 -0300

    Inicialización de proyecto

---
+++ back/healthapp/src/main/java/com/healthcare/HealthApp.java
@@ -0,0 +1,13 @@
+package com.healthcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthApp.class, args);
+	}
+
+}

2d2575907b950dc79c665ac48bda4f1de731c275
Pamela Gonzalez Sanchez <ventas.pamelaglez@gmail.com>
2025-02-21 16:47:42 -0600

    Upload patient entity

---
+++ back/healthapp/src/main/java/com/healthcare/domain/model/entity/Patient.java
@@ -0,0 +1,35 @@
+package com.healthcare.domain.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity(name = "Patient")
+@Table(name = "patients")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private Integer document;
+    @Column(nullable = false)
+    private LocalDate birthDate;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+    private String phone;
+    private String address;
+    private String emergencyContactInfo;
+}
+
+enum Gender {
+    FEMALE,
+    MALE
+}

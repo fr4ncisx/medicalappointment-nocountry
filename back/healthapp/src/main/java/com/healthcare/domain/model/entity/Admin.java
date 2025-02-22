fbb74a901b5929e9feb2d0ef3648e3775327dd7e
Angel Arellano <as.angel.armando@gmail.com>
2025-02-21 18:06:27 -0600

    Upload Admin entity

--- back/healthapp/src/main/java/com/healthcare/domain/model/entity/Admin.java
+++ back/healthapp/src/main/java/com/healthcare/domain/model/entity/Admin.java
@@ -1,5 +1,21 @@
 package com.healthcare.domain.model.entity;
 
-public class Admin {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Integer id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
 }

21833e8ef5fc02af895fa24b4c691662ea039d24
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-02-24 16:44:20 -0300

    feat: add Navbar and Footer components on Layout component

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -1,9 +1,15 @@
 import { Outlet } from "react-router";
+import { Footer } from "./Footer/Footer";
+import { Navbar } from "./Navbar/Navbar";
 
 export const Layout = () => {
-    return(
+    return (
         <>
-            <Outlet/>
+            <Navbar />
+            <main>
+                <Outlet />
+            </main>
+            <Footer />
         </>
     );
 }

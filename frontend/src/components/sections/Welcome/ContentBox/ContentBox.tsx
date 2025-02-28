40829a138d064285cff268f02ae4b0644de1df3f
JuliCoronel <juliecoronell26@gmail.com>
2025-02-27 23:55:45 -0300

    svg Waves agregado

--- frontend/src/components/sections/Welcome/ContentBox/ContentBox.tsx
+++ frontend/src/components/sections/Welcome/ContentBox/ContentBox.tsx
@@ -2,12 +2,26 @@ import { Box } from "@mui/material";
 import { Details } from "./Details/Details";
 import { Gallery } from "./Gallery/Gallery";
 import { ContentBoxStyle } from "./ContentBoxStyle";
+import svg from "./img/wavesOpacity.svg";
 
 export const ContentBox = () => {
     return (
+        <>
+        <img src={svg} alt="Ondas verdes" style={{
+                                    width: "100%",
+                                    transform: "rotate(180deg)",
+                                    display: "block",
+                                    backgroundColor: "#dadada",
+                                    }}/>
         <Box sx={ContentBoxStyle.box}>
             <Gallery />
             <Details />
         </Box>
+        <img src={svg} alt="Ondas verdes" style={{
+                                    width: "100%",
+                                    backgroundColor: "#f1f1f1",
+                                    display: "block",
+                                    }}/>
+        </>
     );
 }
--- frontend/src/components/sections/Welcome/ContentBox/ContentBoxStyle.ts
+++ frontend/src/components/sections/Welcome/ContentBox/ContentBoxStyle.ts
@@ -8,6 +8,5 @@ export const ContentBoxStyle: ObjectStyles = {
         backgroundColor: "#198751",
         width: "100%",
         height: "500px",
-        margin: "5rem 0px 20rem"
-    }
+    },
 };

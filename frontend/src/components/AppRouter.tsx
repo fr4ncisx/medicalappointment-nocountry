e346da39fde943b25192b9e1bc77ec3d3ecfac54
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-02-28 15:27:42 -0300

    feat: se añadio la ruta hacia la seccion MedicosDisponibles

--- frontend/src/components/AppRouter.tsx
+++ frontend/src/components/AppRouter.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import { Layout } from "./layout/Layout";
 import { Welcome } from "./sections/Welcome/Welcome";
+import { MedicosDisponiblesSection } from "./sections/MedicosDisponibles/MedicosDisponiblesSection";
 
 export const AppRouter = () => {
     return (
@@ -8,6 +9,7 @@ export const AppRouter = () => {
             <Routes>
                 <Route path="*" element={<>ERROR 404: page not found</>} />
                 <Route element={<Layout />}>
+                    <Route path="/medicos-disponibles" element={<MedicosDisponiblesSection />} />
                     <Route path="/" element={<Welcome />} />
                 </Route>
             </Routes>
---
+++ frontend/src/components/sections/MedicosDisponibles/MedicosDisponiblesSection.tsx
@@ -0,0 +1,8 @@
+import { Box } from "@mui/material";
+
+export const MedicosDisponiblesSection = () => {
+    return (
+        <Box sx={{ minHeight: "calc(100vh - 90px)" }}>
+        </Box>
+    );
+}

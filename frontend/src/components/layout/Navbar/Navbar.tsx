071349291c30a475fa27906560574a33c3672659
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-02-28 11:46:14 -0300

    feat: se agrego funcionalidad en los botones de registro e inicio de sesion

--- frontend/src/components/layout/Navbar/Navbar.tsx
+++ frontend/src/components/layout/Navbar/Navbar.tsx
@@ -1,21 +1,39 @@
 import { Box, Typography } from "@mui/material";
+import { useModalStore } from "@store/modal.store";
 import { Anchor } from "@ui/Anchor/Anchor";
 import { CustomButton } from "@ui/CustomButton/CustomButton";
 import { NavbarStyles } from "./NavbarStyles";
 
 export const Navbar = () => {
+    const setModalData = useModalStore((state) => state.setModalData);
+
+    const handleClickLogin = () => {
+        setModalData({
+            title: "Iniciar Sesión",
+            operation: "login",
+            showModal: true,
+        });
+    };
+    const handleClickSignUp = () => {
+        setModalData({
+            title: "Registrarse",
+            operation: "sign up",
+            showModal: true,
+        })
+    };
+
     return (
         <Box sx={NavbarStyles.nav}>
             <Box sx={NavbarStyles.logo} />
             <Box sx={NavbarStyles.items}>
                 <Anchor to="/medicos-disponibles" ariaLabel="navegar a la pagina de medicos disponibles">
-                    <Typography fontFamily="Inria Sans Bold">Médicos Disponibles</Typography>
+                    <Typography fontWeight="bold">Médicos Disponibles</Typography>
                 </Anchor>
-                <Anchor to="#" role="button" ariaLabel="navegar a la ventana de registro de usuarios">
-                    <Typography fontFamily="Inria Sans Bold">Registrarse</Typography>
+                <Anchor to="#" onClick={handleClickSignUp} role="button" ariaLabel="navegar a la ventana de registro de usuarios">
+                    <Typography fontWeight="bold">Registrarse</Typography>
                 </Anchor>
                 <CustomButton>
-                    <Typography fontFamily="Inria Sans Bold" textTransform="none">Iniciar Sesión</Typography>
+                    <Typography onClick={handleClickLogin} fontWeight="bold" textTransform="none">Iniciar Sesión</Typography>
                 </CustomButton>
             </Box>
         </Box>

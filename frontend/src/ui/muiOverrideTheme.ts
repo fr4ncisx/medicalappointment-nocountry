a15e9aa9f22d1d403faf01c2748ccda0153d4848
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-03-01 12:31:45 -0300

    refactor: se sobreescribieron los estilos del componente Input de MUI

--- frontend/src/ui/muiOverrideTheme.ts
+++ frontend/src/ui/muiOverrideTheme.ts
@@ -1,20 +1,53 @@
 import { createTheme } from "@mui/material";
 
 export const theme = createTheme({
-    palette: {
-      primary: {
-        main: "#198751",
-        // light: will be calculated from palette.primary.main,
-        // dark: will be calculated from palette.primary.main,
-        // contrastText: will be calculated to contrast with palette.primary.main
+  palette: {
+    primary: {
+      main: "#198751",
+      // light: will be calculated from palette.primary.main,
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    }
+  },
+  components: {
+    MuiTypography: {
+      defaultProps: {
+        fontFamily: "Inria Sans",
+        fontWeight: 400
       }
     },
-    components: {
-      MuiTypography: {
-        defaultProps: {
-          fontFamily: "Inria Sans",
-          fontWeight: 400
-        }
+    MuiFormLabel: {
+      styleOverrides: {
+        root: {
+          '&.Mui-focused': {
+            color: "#25C274", // Color del label cuando el input está enfocado
+          },
+        },
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          '&:hover .MuiOutlinedInput-notchedOutline': {
+            borderColor: "#4DDB94", // Color del borde al hacer hover
+          }
+        },
+      },
+    },
+    MuiFormControl: {
+      styleOverrides: {
+        root: {
+          minHeight: "90px",
+        },
+      },
+    },
+    MuiFormHelperText: {
+      styleOverrides: {
+        root: {
+          margin: "0",
+          padding: "0"
+        },
       }
     }
-  });
+  }
+});

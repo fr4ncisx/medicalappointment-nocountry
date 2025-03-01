3f974aa546bea629af3a863dce9558a18c3fa171
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-03-01 14:58:42 -0300

    fix: cambio de nombre invalido de una prop por un nombre valido

--- frontend/src/components/layout/Footer/Logo.tsx
+++ frontend/src/components/layout/Footer/Logo.tsx
@@ -1,6 +1,6 @@
 
 export const Logo = () => (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0.4 10.4 41.2 52.2" width={"50px"} height={"50px"}>
-        <path d="M16 11H26L28 13V29H39L41 31V43L39 45H28V60L26 62H16L14 60V45H3L1 43V31L3 29H14V13ZM16 31H26L27 44H15Z" stroke="#4DDB94" stroke-width=".6" fill="#198751" />
+        <path d="M16 11H26L28 13V29H39L41 31V43L39 45H28V60L26 62H16L14 60V45H3L1 43V31L3 29H14V13ZM16 31H26L27 44H15Z" stroke="#4DDB94" strokeWidth=".6" fill="#198751" />
     </svg>
 );

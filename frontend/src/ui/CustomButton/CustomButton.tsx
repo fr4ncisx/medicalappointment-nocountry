7132c1dde4beb55ce5c81da1c474a53fa7ebf043
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-03-01 11:38:37 -0300

    feat: se añadio la prop type en el componente CustomButton

--- frontend/src/ui/CustomButton/CustomButton.tsx
+++ frontend/src/ui/CustomButton/CustomButton.tsx
@@ -1,8 +1,8 @@
 import { Button, ButtonProps } from "@mui/material";
 
-export const CustomButton = ({ children, onClick = undefined, variant = "contained", color = "primary" }: ButtonProps) => {
+export const CustomButton = ({ children, onClick = undefined, variant = "contained", color = "primary", type="button" }: ButtonProps) => {
     return (
-        <Button variant={variant} color={color} onClick={onClick} disableElevation={true}>
+        <Button variant={variant} color={color} onClick={onClick} disableElevation={true} type={type}>
             {children}
         </Button>
     );

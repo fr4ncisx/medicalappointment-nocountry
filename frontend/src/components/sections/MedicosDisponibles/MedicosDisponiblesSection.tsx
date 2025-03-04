0d6261389861102928b43eaa6921e96dd7dca775
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-03-04 14:28:27 -0300

    refactor: ahora se puede abrir el modal de login/sign up en medicos disponibles

--- frontend/src/components/sections/MedicosDisponibles/MedicosDisponiblesSection.tsx
+++ frontend/src/components/sections/MedicosDisponibles/MedicosDisponiblesSection.tsx
@@ -6,6 +6,8 @@ import { MedicosDisponiblesStyles } from "./MedicosDisponiblesStyles";
 import SearchBar from "./Components/SearchBar";
 import DoctorCard from "./Components/DoctorCard";
 import Filters from "./Components/Filters";
+import ModalMenu from "../Welcome/ModalMenu/ModalMenu";
+import CustomModal from "@ui/CustomModal/CustomModal";
 
 interface Doctor {
   id: number;
@@ -23,35 +25,40 @@ export const MedicosDisponiblesSection = () => {
   }, []);
 
   return (
-    <Box sx={{ minHeight: "calc(100vh - 90px)", display: "flex" }}>
-      {/* Box Izquierdo - Container */}
-      <Box sx={MedicosDisponiblesStyles.asideContainer}>
-        <SearchBar />
-        {/* Box - Filtros */}
-        <Filters />
-        {/* Boton - Mostrar Doctores */}
-        <Box
-          sx={{ display: "flex", justifyContent: "center", marginTop: "2rem" }}
-        >
-          <CustomButton>
-            {/* OnClick={} debería pedir los doctores teniendo en cuenta los filtros. */}
-            <Typography
-              fontFamily="Inria Sans Bold"
-              textTransform="none"
-              fontSize={"1.4rem"}
-            >
-              Mostrar Doctores
-            </Typography>
-          </CustomButton>
+    <>
+      <Box sx={{ minHeight: "calc(100vh - 90px)", display: "flex" }}>
+        {/* Box Izquierdo - Container */}
+        <Box sx={MedicosDisponiblesStyles.asideContainer}>
+          <SearchBar />
+          {/* Box - Filtros */}
+          <Filters />
+          {/* Boton - Mostrar Doctores */}
+          <Box
+            sx={{ display: "flex", justifyContent: "center", marginTop: "2rem" }}
+          >
+            <CustomButton>
+              {/* OnClick={} debería pedir los doctores teniendo en cuenta los filtros. */}
+              <Typography
+                fontFamily="Inria Sans Bold"
+                textTransform="none"
+                fontSize={"1.4rem"}
+              >
+                Mostrar Doctores
+              </Typography>
+            </CustomButton>
+          </Box>
+        </Box>
+
+        {/* Box Derecho - Grid Doctores */}
+        <Box sx={MedicosDisponiblesStyles.gridContainer}>
+          {doctors.map((doctor) => (
+            <DoctorCard key={doctor.id} doctor={doctor} />
+          ))}
         </Box>
       </Box>
-
-      {/* Box Derecho - Grid Doctores */}
-      <Box sx={MedicosDisponiblesStyles.gridContainer}>
-        {doctors.map((doctor) => (
-          <DoctorCard key={doctor.id} doctor={doctor} />
-        ))}
-      </Box>
-    </Box>
+      <CustomModal>
+        <ModalMenu />
+      </CustomModal>
+    </>
   );
 };

778300df8e933730fe96a54ecba1f55302c43e2c
fr4ncisx <fr4ncisx@gmail.com>
2025-03-04 11:14:53 -0300

    feat: Created a new DTO User that implements UserDetails for Authentication, to prevent security issues using User from Entity

---
+++ back/healthapp/src/main/java/com/healthcare/infrastructure/dto/request/UserAuthenticated.java
@@ -0,0 +1,40 @@
+package com.healthcare.infrastructure.dto.request;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.healthcare.domain.model.entity.User;
+import com.healthcare.domain.model.enums.Role;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class UserAuthenticated implements UserDetails{
+    private String email;
+    private String password;
+    private Role role;
+
+    public UserAuthenticated(User user) {
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+        this.role = user.getRole();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(role);
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+}

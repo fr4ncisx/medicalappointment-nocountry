c0a9b6fa8f6ce6ccd1a5cd8930f5cf0f6c6d580f
fr4ncisx <fr4ncisx@gmail.com>
2025-03-04 11:13:58 -0300

    fix: updated mistaken attribute username

--- back/healthapp/src/main/java/com/healthcare/infrastructure/dto/request/RequestLoginDTO.java
+++ back/healthapp/src/main/java/com/healthcare/infrastructure/dto/request/RequestLoginDTO.java
@@ -3,4 +3,4 @@ package com.healthcare.infrastructure.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record RequestLoginDTO(@NotNull @NotBlank String username, @NotNull @NotBlank String password) {}
+public record RequestLoginDTO(@NotNull @NotBlank String email, @NotNull @NotBlank String password) {}

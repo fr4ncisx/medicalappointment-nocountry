955a07bcb66af16bab95b475733b54c4a4ec48a4
fr4ncisx <fr4ncisx@gmail.com>
2025-03-06 15:13:12 -0300

    fix: customized documentation response

--- back/healthapp/src/main/java/com/healthcare/infrastructure/security/service/AuthController.java
+++ back/healthapp/src/main/java/com/healthcare/infrastructure/security/service/AuthController.java
@@ -1,5 +1,7 @@
 package com.healthcare.infrastructure.security.service;
 
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.healthcare.infrastructure.dto.request.RequestLoginDTO;
+import com.healthcare.infrastructure.meta.DocumentedResponse;
 
 import jakarta.validation.Valid;
 
@@ -18,8 +21,11 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
+    @DocumentedResponse(responseCode = "200",
+    description = "Respuesta de login correcta",
+    content = @Content(schema = @Schema(example = "{\"token\": \"jwt_token\"}")))
     @PostMapping("/login")
-    public ResponseEntity<?> loggedIn(@RequestBody @Valid RequestLoginDTO login){
+    public ResponseEntity<?> loggedIn(@RequestBody @Valid RequestLoginDTO login) {
         return authService.loginUser(login);
     }
 

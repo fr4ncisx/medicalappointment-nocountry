f15b2b5a144214821ce148c340bd80431113f979
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-03-07 12:30:17 -0300

    refactor: se mejoro el rendimiento al controlar el renderizado del modal

--- frontend/src/components/sections/Welcome/Welcome.tsx
+++ frontend/src/components/sections/Welcome/Welcome.tsx
@@ -2,15 +2,22 @@ import CustomModal from "@ui/CustomModal/CustomModal";
 import { ContentBox } from "./ContentBox/ContentBox";
 import { MainBox } from "./MainBox/MainBox";
 import ModalMenu from "./ModalMenu/ModalMenu";
+import { useModalStore } from "@store/modal.store";
 
 export const Welcome = () => {
+    const showModal = useModalStore((state) => state.modalData.showModal);
+
     return (
         <>
             <MainBox />
             <ContentBox />
-            <CustomModal>
-                <ModalMenu />
-            </CustomModal>
+            {
+                showModal && (
+                    <CustomModal>
+                        <ModalMenu />
+                    </CustomModal>
+                )
+            }
         </>
     );
 }

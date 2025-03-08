aa6a0699afcd824452343a8275ac293a24b2d3d7
fr4ncisx <fr4ncisx@gmail.com>
2025-03-07 22:11:57 -0300

    feat: added boolean validation email exists to invalidate any new user request

--- back/healthapp/src/main/java/com/healthcare/domain/repository/UserRepository.java
+++ back/healthapp/src/main/java/com/healthcare/domain/repository/UserRepository.java
@@ -10,5 +10,6 @@ import com.healthcare.domain.model.entity.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
     Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
     
 }

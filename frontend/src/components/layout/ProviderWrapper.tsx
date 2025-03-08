91ed3d39a0b8dce931d7c77a8c78f0500f1231e4
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-03-08 14:15:02 -0300

    feat: se cambio el idioma de MUI al español

--- frontend/src/components/layout/ProviderWrapper.tsx
+++ frontend/src/components/layout/ProviderWrapper.tsx
@@ -2,13 +2,21 @@ import { ThemeProvider } from '@mui/material';
 import { theme } from '@ui/muiOverrideTheme';
 import ErrorBoundary from '@components/layout/ErrorBoundary';
 import { ReactNode } from 'react';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3';
+import { es } from "date-fns/locale/es";
+import { esES } from '@mui/x-date-pickers/locales';
+
+const spanishLocale = esES.components.MuiLocalizationProvider.defaultProps.localeText;
 
 export const ProviderWrapper = ({ children }: { children: ReactNode }) => {
     return (
         <ErrorBoundary fallback={<>Ocurrio un error desconocido, fijate en consola que dice</>}>
-            <ThemeProvider theme={theme}>
-                {children}
-            </ThemeProvider>
+            <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={es} localeText={spanishLocale}>
+                <ThemeProvider theme={theme}>
+                    {children}
+                </ThemeProvider>
+            </LocalizationProvider>
         </ErrorBoundary>
     );
 }
--- frontend/src/ui/muiOverrideTheme.ts
+++ frontend/src/ui/muiOverrideTheme.ts
@@ -1,65 +1,69 @@
 import { createTheme } from "@mui/material";
+import { esES } from "@mui/x-date-pickers/locales";
 
-export const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#198751",
-      // light: will be calculated from palette.primary.main,
-      // dark: will be calculated from palette.primary.main,
-      // contrastText: will be calculated to contrast with palette.primary.main
-    }
-  },
-  components: {
-    MuiTypography: {
-      defaultProps: {
-        fontFamily: "Inria Sans",
-        fontWeight: 400,
-        variantMapping: {
-          h1: 'h2',
-          h2: 'h2',
-          h3: 'h2',
-          h4: 'h2',
-          h5: 'h2',
-          h6: 'h2',
-          subtitle1: 'h2',
-          subtitle2: 'h2',
-          body1: 'p',
-          body2: 'span',
-        },
+export const theme = createTheme(
+  {
+    palette: {
+      primary: {
+        main: "#198751",
+        // light: will be calculated from palette.primary.main,
+        // dark: will be calculated from palette.primary.main,
+        // contrastText: will be calculated to contrast with palette.primary.main
       }
     },
-    MuiFormLabel: {
-      styleOverrides: {
-        root: {
-          '&.Mui-focused': {
-            color: "#25C274", // Color del label cuando el input está enfocado
+    components: {
+      MuiTypography: {
+        defaultProps: {
+          fontFamily: "Inria Sans",
+          fontWeight: 400,
+          variantMapping: {
+            h1: 'h2',
+            h2: 'h2',
+            h3: 'h2',
+            h4: 'h2',
+            h5: 'h2',
+            h6: 'h2',
+            subtitle1: 'h2',
+            subtitle2: 'h2',
+            body1: 'p',
+            body2: 'span',
+          },
+        }
+      },
+      MuiFormLabel: {
+        styleOverrides: {
+          root: {
+            '&.Mui-focused': {
+              color: "#25C274", // Color del label cuando el input está enfocado
+            },
           },
         },
       },
-    },
-    MuiOutlinedInput: {
-      styleOverrides: {
-        root: {
-          '&:hover .MuiOutlinedInput-notchedOutline': {
-            borderColor: "#4DDB94", // Color del borde al hacer hover
-          }
+      MuiOutlinedInput: {
+        styleOverrides: {
+          root: {
+            '&:hover .MuiOutlinedInput-notchedOutline': {
+              borderColor: "#4DDB94", // Color del borde al hacer hover
+            }
+          },
         },
       },
-    },
-    MuiFormControl: {
-      styleOverrides: {
-        root: {
-          minHeight: "90px",
+      MuiFormControl: {
+        styleOverrides: {
+          root: {
+            minHeight: "90px",
+          },
         },
       },
-    },
-    MuiFormHelperText: {
-      styleOverrides: {
-        root: {
-          margin: "0",
-          padding: "0"
-        },
+      MuiFormHelperText: {
+        styleOverrides: {
+          root: {
+            margin: "0",
+            padding: "0"
+          },
+        }
       }
     }
-  }
-});
+  },
+  esES
+);

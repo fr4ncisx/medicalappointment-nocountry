9eeced786a3dea655faaf8aa272af215152a77a6
fr4ncisx <fr4ncisx@gmail.com>
2025-03-07 22:15:42 -0300

    feat: added UserRequestDTO and added User Constructor to receive dto, password and role parameters

---
+++ back/healthapp/src/main/java/com/healthcare/domain/dto/UserRequestDTO.java
@@ -0,0 +1,18 @@
+package com.healthcare.domain.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRequestDTO {
+    @NotNull @NotBlank @Email
+    private String email;
+    @NotNull @NotBlank
+    private String password;
+}

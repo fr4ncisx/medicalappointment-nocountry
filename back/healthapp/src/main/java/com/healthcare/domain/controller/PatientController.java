7fca74fab9c0932a42b9caa905915f342d4e9cda
fr4ncisx <fr4ncisx@gmail.com>
2025-03-07 22:09:32 -0300

    feat: Role assignation in controller

--- back/healthapp/src/main/java/com/healthcare/domain/controller/PatientController.java
+++ back/healthapp/src/main/java/com/healthcare/domain/controller/PatientController.java
@@ -1,34 +1,39 @@
 package com.healthcare.domain.controller;
 
-import com.healthcare.domain.dto.PatientDTO;
+import com.healthcare.domain.dto.PatientRequestDTO;
 import com.healthcare.domain.service.IPatientService;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/patient")
 public class PatientController {
 
-    @Autowired
-    private IPatientService patientService;
+    private final IPatientService patientService;
 
+    @PreAuthorize("hasAnyRole({'ADMIN', 'MEDICO'})")
     @GetMapping
     public ResponseEntity<?> getAllPatients(){
         return patientService.getAllPatients();
     }
-
+    
+    @PreAuthorize("hasAnyRole({'ADMIN', 'PACIENTE'})")
     @GetMapping("/{id}")
     public ResponseEntity<?> getPatientById(@PathVariable Long id){
         return patientService.getPatientById(id);
     }
-
+    
     @PostMapping
-    public ResponseEntity<?> createPatient(@RequestBody @Valid PatientDTO patientDTO) {
+    public ResponseEntity<?> createPatient(@RequestBody @Valid PatientRequestDTO patientDTO) {
         return patientService.createPatient(patientDTO);
     }
 
+    @PreAuthorize("hasAnyRole({'ADMIN','MEDICO'})")
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deletePatient(@PathVariable Long id) {
         return patientService.deletePatient(id);

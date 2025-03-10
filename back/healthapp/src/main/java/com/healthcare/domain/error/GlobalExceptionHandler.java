29cf3923f9d6a62140354bb874f25de67e3e9991
fr4ncisx <fr4ncisx@gmail.com>
2025-03-09 21:26:31 -0300

    fix: updated methods with sonarQube

--- back/healthapp/src/main/java/com/healthcare/domain/error/GlobalExceptionHandler.java
+++ back/healthapp/src/main/java/com/healthcare/domain/error/GlobalExceptionHandler.java
@@ -1,20 +1,25 @@
 package com.healthcare.domain.error;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.healthcare.domain.exceptions.*;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final String STATUS_ERROR = "ERROR";
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<Map<String, String>> handleException(HttpMessageNotReadableException ex) {
@@ -34,84 +39,90 @@ public class GlobalExceptionHandler {
                 .findFirst()
                 .orElse("Error en la solicitud. Verifique el formato del JSON.");
     
-        errors.put("error", errorMsg);
+        errors.put(STATUS_ERROR, errorMsg);
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<Map<String, String>> missingServlet(MissingServletRequestParameterException ex) {
+        return new ResponseEntity<>(Map.of(STATUS_ERROR, ex.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(AuthorizationDeniedException.class)
-    public ResponseEntity<?> deniedAccess(AuthorizationDeniedException ex) {
-        return new ResponseEntity<>(Map.of("error", ex.getMessage()), HttpStatus.UNAUTHORIZED);
+    public ResponseEntity<Map<String, String>> deniedAccess(AuthorizationDeniedException ex) {
+        return new ResponseEntity<>(Map.of(STATUS_ERROR, ex.getMessage()), HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<?> illegalArgument(IllegalArgumentException ex) {
-        return new ResponseEntity<>(Map.of("error", ex.getMessage()), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Map<String, String>> illegalArgument(IllegalArgumentException ex) {
+        return new ResponseEntity<>(Map.of(STATUS_ERROR, ex.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-    public ResponseEntity<?> mediaTypeNotSupportedEx(HttpMediaTypeNotSupportedException ex) {
-        return new ResponseEntity<>(Map.of("error", ex.getMessage()), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Map<String, String>> mediaTypeNotSupportedEx(HttpMediaTypeNotSupportedException ex) {
+        return new ResponseEntity<>(Map.of(STATUS_ERROR, ex.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(NotFoundInDatabaseException.class)
-    public ResponseEntity<?> notFoundInDataBase(NotFoundInDatabaseException ex) {
-        return new ResponseEntity<>(Map.of("error", ex.getMessage()), HttpStatus.NOT_FOUND);
+    public ResponseEntity<Map<String, String>> notFoundInDataBase(NotFoundInDatabaseException ex) {
+        return new ResponseEntity<>(Map.of(STATUS_ERROR, ex.getMessage()), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(MedicNotFoundException.class)
-    public ResponseEntity<?> medicNotFound(MedicNotFoundException ex) {
-        return new ResponseEntity<>(Map.of("error", ex.getMessage()), HttpStatus.NOT_FOUND);
+    public ResponseEntity<Map<String, String>> medicNotFound(MedicNotFoundException ex) {
+        return new ResponseEntity<>(Map.of(STATUS_ERROR, ex.getMessage()), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(PatientNotFoundException.class)
-    public ResponseEntity<?> patientNotFound(PatientNotFoundException ex) {
-        return new ResponseEntity<>(Map.of("error", ex.getMessage()), HttpStatus.NOT_FOUND);
+    public ResponseEntity<Map<String, String>> patientNotFound(PatientNotFoundException ex) {
+        return new ResponseEntity<>(Map.of(STATUS_ERROR, ex.getMessage()), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(MedicScheduleConflictException.class)
-    public ResponseEntity<?> medicScheduleConflict(MedicScheduleConflictException ex) {
-        return new ResponseEntity<>(Map.of("error", ex.getMessage()), HttpStatus.CONFLICT);
+    public ResponseEntity<Map<String, String>> medicScheduleConflict(MedicScheduleConflictException ex) {
+        return new ResponseEntity<>(Map.of(STATUS_ERROR, ex.getMessage()), HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(AppointmentNotFoundException.class)
-    public ResponseEntity<?> appointmentNotFound(AppointmentNotFoundException ex) {
-        return new ResponseEntity<>(Map.of("error", ex.getMessage()), HttpStatus.NOT_FOUND);
+    public ResponseEntity<Map<String, String>> appointmentNotFound(AppointmentNotFoundException ex) {
+        return new ResponseEntity<>(Map.of(STATUS_ERROR, ex.getMessage()), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(AppointmentDateConflictException.class)
-    public ResponseEntity<?> appointmentDateConflict(AppointmentDateConflictException ex) {
-        return new ResponseEntity<>(Map.of("error", ex.getMessage()), HttpStatus.CONFLICT);
+    public ResponseEntity<Map<String, String>> appointmentDateConflict(AppointmentDateConflictException ex) {
+        return new ResponseEntity<>(Map.of(STATUS_ERROR, ex.getMessage()), HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(CancelledAppointmentException.class)
-    public ResponseEntity<?> cancelledAppointment(CancelledAppointmentException ex) {
-        return new ResponseEntity<>(Map.of("error", ex.getMessage()), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Map<String, String>> cancelledAppointment(CancelledAppointmentException ex) {
+        return new ResponseEntity<>(Map.of(STATUS_ERROR, ex.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(InvalidDataException.class)
-    public ResponseEntity<?> invalidDataException(InvalidDataException ex) {
-        return new ResponseEntity<>(Map.of("error", ex.getMessage()), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Map<String, String>> invalidDataException(InvalidDataException ex) {
+        return new ResponseEntity<>(Map.of(STATUS_ERROR, ex.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(DuplicatedEntryEx.class)
-    public ResponseEntity<?> duplicateException(DuplicatedEntryEx ex) {
-        return new ResponseEntity<>(Map.of("error", ex.getMessage()), HttpStatus.CONFLICT);
+    public ResponseEntity<Map<String, String>> duplicateException(DuplicatedEntryEx ex) {
+        return new ResponseEntity<>(Map.of(STATUS_ERROR, ex.getMessage()), HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(InvalidFieldException.class)
-    public ResponseEntity<?> invalidFieldException(InvalidFieldException ex) {
-        return new ResponseEntity<>(Map.of("error", ex.getMessage()), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Map<String, String>> invalidFieldException(InvalidFieldException ex) {
+        return new ResponseEntity<>(Map.of(STATUS_ERROR, ex.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MedicDeletionException.class)
-    public ResponseEntity<?> medicDeleteException(MedicDeletionException ex) {
-        return new ResponseEntity<>(Map.of("error", ex.getMessage()), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Map<String, String>> medicDeleteException(MedicDeletionException ex) {
+        return new ResponseEntity<>(Map.of(STATUS_ERROR, ex.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> validationErrors(MethodArgumentNotValidException ex) {
-        var listOfErrors = ex.getFieldErrors().stream()
-                .map(e -> new ShowFieldErrors(e.getField(), e.getDefaultMessage()));
+    public ResponseEntity<List<ShowFieldErrors>> validationErrors(MethodArgumentNotValidException ex) {
+        List<ShowFieldErrors> listOfErrors = ex.getFieldErrors().stream()
+            .map(e -> new ShowFieldErrors(e.getField(), e.getDefaultMessage()))
+            .toList();
         return new ResponseEntity<>(listOfErrors, HttpStatus.BAD_REQUEST);
     }
 

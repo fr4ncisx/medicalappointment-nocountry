b7ff4a1d4450475fc41bc16e436ecb9060434298
fr4ncisx <fr4ncisx@gmail.com>
2025-03-10 21:09:22 -0300

    bug: fixed response when cannot deserialize expected Object and actual String or other

--- back/healthapp/src/main/java/com/healthcare/domain/error/GlobalExceptionHandler.java
+++ back/healthapp/src/main/java/com/healthcare/domain/error/GlobalExceptionHandler.java
@@ -26,11 +26,14 @@ public class GlobalExceptionHandler {
         Map<String, String> errors = new HashMap<>();
     
         Map<String, String> errorPatterns = Map.of(
-                "Required request body is missing", "El cuerpo de la consulta está vacío",
-                "was expecting comma to separate", "Falta coma para separar algunos de los atributos",
-                "expected close marker for Object", "Se espera cierre de llave al final",
-                "Cannot deserialize value of type", "Hubo un error al deserializar, se esperaba solo un objeto y no un arreglo",
-                "Cannot construct instance of", "Error de formato en la solicitud"
+            "Required request body is missing", "El cuerpo de la consulta está vacío",
+            "was expecting comma to separate", "Falta coma para separar algunos de los atributos",
+            "expected close marker for Object", "Se espera cierre de llave al final",
+            "not one of the values accepted for Enum class:", 
+                "Error de deserialización: el valor " + ex.getLocalizedMessage().split("\"")[1] + 
+                " no es uno de los valores aceptados. Valores esperados: " + 
+                ex.getLocalizedMessage().substring(ex.getLocalizedMessage().indexOf("[")),
+            "Cannot construct instance of", "Error de formato en la solicitud"
         );
     
         String errorMsg = errorPatterns.entrySet().stream()

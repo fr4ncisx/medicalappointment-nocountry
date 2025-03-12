2f3b7e12779dc72406e80761b67121a6f1c4abb8
fr4ncisx <fr4ncisx@gmail.com>
2025-03-11 22:00:28 -0300

    bug: fixed schedule not adding properly

--- back/healthapp/src/main/java/com/healthcare/domain/repository/ScheduleRepository.java
+++ back/healthapp/src/main/java/com/healthcare/domain/repository/ScheduleRepository.java
@@ -4,11 +4,7 @@ import com.healthcare.domain.model.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-    boolean existsByMedicIdAndStartDateLessThanEqualAndEndDateGreaterThanEqualAndStartTimeLessThanEqualAndEndTimeGreaterThanEqual(
-            Long medicId, LocalDate startDate, LocalDate endDate, LocalTime startTime, LocalTime endTime);
+
 }
--- back/healthapp/src/main/java/com/healthcare/domain/service/ScheduleServiceImpl.java
+++ back/healthapp/src/main/java/com/healthcare/domain/service/ScheduleServiceImpl.java
@@ -31,22 +31,13 @@ public class ScheduleServiceImpl implements IScheduleService {
     @Transactional
     public ResponseEntity<?> createSchedule(Long medicId, ScheduleRequest scheduleRequest) {
         Medic medic = getMedic(medicId);
-
-        boolean existSchedule = scheduleRepository.
-                existsByMedicIdAndStartDateLessThanEqualAndEndDateGreaterThanEqualAndStartTimeLessThanEqualAndEndTimeGreaterThanEqual(
-                medicId,
-                scheduleRequest.getStartDate(), scheduleRequest.getEndDate(),
-                scheduleRequest.getStartTime(), scheduleRequest.getEndTime()
-        );
-
-        if(existSchedule) {
-            throw new InvalidDataException("El médico ya tiene un horario en este rango de fechas");
-        }
-
-        Schedule schedule = new Schedule(scheduleRequest, medic);
-        scheduleRepository.save(schedule);
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(Response.create("message", "Horario creado con éxito"));
+        var mediSchedule = medic.getSchedules();
+        checkTakenSchedule(mediSchedule, scheduleRequest);
+        Schedule newSchedule = new Schedule(scheduleRequest, medic);
+        scheduleRepository.saveAndFlush(newSchedule);
+        var response = modelMapper.map(newSchedule, ScheduleResponse.class);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(response);
     }
 
     @Override
@@ -55,7 +46,7 @@ public class ScheduleServiceImpl implements IScheduleService {
 
         List<Schedule> schedules = medic.getSchedules();
 
-        if(schedules.isEmpty()) {
+        if (schedules.isEmpty()) {
             throw new NotFoundInDatabaseException("No se encontraron horarios para este médico");
         }
 
@@ -78,14 +69,12 @@ public class ScheduleServiceImpl implements IScheduleService {
     @Override
     @Transactional
     public ResponseEntity<?> updateSchedule(Long scheduleId, ScheduleRequest scheduleRequest) {
-        Schedule schedule = getSchedule(scheduleId);
+        Schedule sch = getSchedule(scheduleId);
+        checkTakenSchedule(sch, scheduleRequest);
+        modelMapper.map(scheduleRequest, sch);
+        scheduleRepository.save(sch);
 
-        Schedule updatedSchedule = new Schedule(scheduleRequest, schedule.getMedic());
-        updatedSchedule.setId(schedule.getId());
-
-        scheduleRepository.save(updatedSchedule);
-
-        ScheduleResponse scheduleResponse = modelMapper.map(updatedSchedule, ScheduleResponse.class);
+        ScheduleResponse scheduleResponse = modelMapper.map(sch, ScheduleResponse.class);
 
         return ResponseEntity.ok(scheduleResponse);
     }
@@ -98,13 +87,55 @@ public class ScheduleServiceImpl implements IScheduleService {
         return ResponseEntity.ok(Response.create("message", "Horario eliminado con éxito"));
     }
 
-    private Schedule getSchedule(Long id){
+    private Schedule getSchedule(Long id) {
         return scheduleRepository.findById(id)
                 .orElseThrow(() -> new NotFoundInDatabaseException("Horario no encontrado"));
     }
 
-    private Medic getMedic(Long id){
+    private Medic getMedic(Long id) {
         return medicRepository.findById(id)
                 .orElseThrow(() -> new MedicNotFoundException("Médico no encontrado"));
     }
+
+    private void validateTime(ScheduleRequest scheduleRequest) {
+        if (scheduleRequest.getStartDate().isAfter(scheduleRequest.getEndDate())) {
+            throw new InvalidDataException("El horario de fin no puede ser menor al horario de inicio");
+        }
+        if (scheduleRequest.getStartTime() == scheduleRequest.getEndTime()) {
+            throw new InvalidDataException("No pueden asignarse los mismos horarios de inicio y fin");
+        }
+        if (scheduleRequest.getEndTime().isBefore(scheduleRequest.getStartTime())) {
+            throw new InvalidDataException("El horario de fin no puede ser menor al horario de inicio");
+        }
+    }
+
+    private void checkTakenSchedule(List<Schedule> mediSchedule, ScheduleRequest scheduleRequest) {
+        validateTime(scheduleRequest);
+        for (Schedule sch : mediSchedule) {
+            boolean isSameDay = sch.getStartDate().isEqual(scheduleRequest.getStartDate()) &&
+                    sch.getEndDate().isEqual(scheduleRequest.getEndDate());
+            if (isSameDay) {
+                boolean timeIsTaken = scheduleRequest.getStartTime().isBefore(sch.getEndTime()) && scheduleRequest.getEndTime().isAfter(sch.getStartTime());
+                if (timeIsTaken) {
+                    throw new InvalidDataException("No se puede crear ese rango horario porque ya existe uno");
+                }
+            }
+
+        }
+    }
+
+
+    // TODO: Faltaria validar que al editar el horario no se superponga con los del mismo día y médico
+    private void checkTakenSchedule(Schedule sch, ScheduleRequest request) {
+        var requestStartTime = request.getStartTime();
+        var requestEndTime = request.getEndTime();
+        validateTime(request);
+        boolean sameDay = sch.getStartDate().isEqual(request.getStartDate()) && sch.getEndDate().isEqual(request.getEndDate());
+        boolean sameTime = requestStartTime.isBefore(sch.getEndTime()) && requestEndTime.isAfter(sch.getStartTime());
+
+        if (sameDay && sameTime) {
+            throw new InvalidDataException("El horario no se puede editar ya que se superpone con otro");
+        }
+    }
+
 }

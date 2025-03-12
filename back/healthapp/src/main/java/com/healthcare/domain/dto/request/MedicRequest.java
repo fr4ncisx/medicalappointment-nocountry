cd69fe28ad22e78d14af664fa03db3fe86544515
fr4ncisx <fr4ncisx@gmail.com>
2025-03-12 15:31:33 -0300

    fix: modified dto for validations

--- back/healthapp/src/main/java/com/healthcare/domain/dto/request/MedicRequest.java
+++ back/healthapp/src/main/java/com/healthcare/domain/dto/request/MedicRequest.java
@@ -1,10 +1,13 @@
 package com.healthcare.domain.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.healthcare.domain.model.enums.Gender;
 import com.healthcare.domain.model.enums.Speciality;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,8 +20,11 @@ public class MedicRequest{
     @NotNull @NotBlank private String lastname;
     @NotNull @NotBlank private String description;
     @NotNull @NotBlank private String state;
-    @NotNull @NotBlank private String documentId;
+    @NotNull @NotBlank @PositiveOrZero
+    private String documentId;
     @NotNull private Gender gender;
     @NotNull private Speciality speciality;
     @NotNull @NotBlank private String phone;
+    @JsonProperty("user")
+    @NotNull @Valid UserRequest user;
 }

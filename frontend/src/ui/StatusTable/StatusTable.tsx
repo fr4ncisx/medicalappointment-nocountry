d30f2df3b79bac6d491dc8f52755a9fe076abc07
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-03-13 12:45:33 -0300

    fix: valor invalido en prop

--- frontend/src/ui/StatusTable/StatusTable.tsx
+++ frontend/src/ui/StatusTable/StatusTable.tsx
@@ -10,7 +10,7 @@ interface Props {
 export const StatusTable = ({ color, label, tableWithTabs = false }: Props) => {
     return (
         <Box>
-            <Box sx={{ ...StatusTableStyles.header, borderTopLeftRadius: tableWithTabs ? "0" : "10" }}></Box>
+            <Box sx={{ ...StatusTableStyles.header, borderTopLeftRadius: tableWithTabs ? "0" : "10px" }}></Box>
             <Box sx={StatusTableStyles.body}>
                 <Typography variant="h5" color={color}>{label}</Typography>
             </Box>

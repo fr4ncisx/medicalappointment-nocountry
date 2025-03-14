def23cb791eb98d99ef48fca28c5f7e4a361ce44
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-03-14 15:35:32 -0300

    fix: error por tener un anchor dentro de otro anchor

--- frontend/src/components/layout/Footer/LogoSection/LogoSection.tsx
+++ frontend/src/components/layout/Footer/LogoSection/LogoSection.tsx
@@ -1,22 +1,16 @@
 import { Box, Typography } from "@mui/material";
-import { Anchor } from "@ui/Anchor/Anchor";
 import { FooterStyle } from "../FooterStyle";
 import { Logo } from "../Logo";
 
 export const LogoSection = () => {
     return (
         <Box sx={FooterStyle.footerTitle}>
-            <Anchor
-                to="#"
-                ariaLabel="logo de la pagina web"
-            >
-                <Box display={"flex"} alignItems={"center"}>
-                    <Logo />
-                    <Typography>
-                        MedicalAppointment
-                    </Typography>
-                </Box>
-            </Anchor>
+            <Box display={"flex"} alignItems={"center"}>
+                <Logo />
+                <Typography>
+                    MedicalAppointment
+                </Typography>
+            </Box>
             <Typography variant="body2" color="textSecondary">
                 © No Country 2025
             </Typography>

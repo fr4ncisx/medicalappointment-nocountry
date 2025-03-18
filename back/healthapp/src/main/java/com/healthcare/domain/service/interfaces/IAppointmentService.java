2babf38c7d714352411d8043baa70c03bd974c1a
fr4ncisx <fr4ncisx@gmail.com>
2025-03-18 15:55:23 -0300

    feat: added new derived query

--- back/healthapp/src/main/java/com/healthcare/domain/service/interfaces/IAppointmentService.java
+++ back/healthapp/src/main/java/com/healthcare/domain/service/interfaces/IAppointmentService.java
@@ -7,7 +7,12 @@ import com.healthcare.domain.dto.request.AppointmentRequest;
 
 public interface IAppointmentService {
     ResponseEntity<?> scheduleAppointment(Long patientId, Long medicId, AppointmentRequest appointmentRequest) throws MessagingException;
+
     ResponseEntity<?> updateAppointment(Long appointmentId, AppointmentRequest appointmentRequest);
+
     ResponseEntity<?> cancelAppointment(Long appointmentId) throws MessagingException;
+
     ResponseEntity<?> getAppointmentsByPatient(Long patientId);
+
+    ResponseEntity<?> getAppointmentsByMedic(Long medicId);
 }

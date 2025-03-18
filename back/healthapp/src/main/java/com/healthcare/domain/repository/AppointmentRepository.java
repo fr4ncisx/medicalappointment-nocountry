5974d687e09eedd0f85129b498a5548c38d9011b
fr4ncisx <fr4ncisx@gmail.com>
2025-03-18 15:55:41 -0300

    feat: added new derived query for time check

--- back/healthapp/src/main/java/com/healthcare/domain/repository/AppointmentRepository.java
+++ back/healthapp/src/main/java/com/healthcare/domain/repository/AppointmentRepository.java
@@ -1,6 +1,7 @@
 package com.healthcare.domain.repository;
 
 import com.healthcare.domain.model.entity.Appointment;
+import com.healthcare.domain.model.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,5 @@ import java.util.List;
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
     List<Appointment> findByPatientId(Long patientId);
     boolean existsByMedicId(Long medicId);
+    List<Appointment> findByMedicIdAndStatusOrderByDateAscTimeAsc(Long medicId, Status status);
 }

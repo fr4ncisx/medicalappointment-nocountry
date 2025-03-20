2e9f3c9d1dc16231cc5158f1035b18c2545cc5e1
fr4ncisx <fr4ncisx@gmail.com>
2025-03-20 11:07:58 -0300

    fix: fixed listener receiving object instead of dto, using Message instead

--- back/healthapp/src/main/java/com/healthcare/domain/service/NotificationListener.java
+++ back/healthapp/src/main/java/com/healthcare/domain/service/NotificationListener.java
@@ -1,6 +1,7 @@
 package com.healthcare.domain.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
@@ -14,12 +15,18 @@ public class NotificationListener {
     /**
      * This receives every rabbitmq message
      * and send it to a private websocket channel
-     * @param userId the user that want to receive the message
-     * @param msg actual message
+     * we request by the securitycontext the userId and then do the validations
+     *the user that want to receive the message
+     * @param message Message from the service
      */
     @RabbitListener(queues = "notification")
-    public void receiveMessage(String userId, String msg) {
-        messagingTemplate.convertAndSend("/user/" + userId + "/notifications", msg);
+    public void receiveMessage(Message message) {
+        String payload = new String(message.getBody());
+        String routingKey = message.getMessageProperties().getReceivedRoutingKey();
+        String userId = routingKey.split("\\.")[2];
+        System.out.println(userId + "/notifications" + payload);
+        System.out.println("Mensaje recibido en RabbitListener: " + payload);
+        messagingTemplate.convertAndSendToUser(userId, "/notifications", payload);
     }
 
 }

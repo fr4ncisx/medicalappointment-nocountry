79515256b4ba366fc0aae59e35b352d79283f8f1
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-03-28 16:10:52 -0300

    refactor: se cambio socket.io-client por stomp js

--- frontend/src/components/MedicoWebSocket.tsx
+++ frontend/src/components/MedicoWebSocket.tsx
@@ -1,80 +1,41 @@
-import { Box, Container, List, ListItem, Typography } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 import { useUserStore } from "@store/user.store";
 import { useEffect, useState } from "react";
-import { io } from "socket.io-client";
-
-interface Notification {
-    id: string
-    text: string
-    isRead: boolean
-    user: any
-}
+import { Client } from '@stomp/stompjs';
 
 export const MedicoWebSocket = () => {
+    const [isConnected, setIsConnected] = useState(false);
     const id = useUserStore(state => state.getUserId)();
-    const [notifications, setNotifications] = useState<Notification[]>([]);
-    const [conection, setConection] = useState(false);
 
-    const socket = io("ws://localhost:8080/ws");
+    const brokerURL = "ws://localhost:8080/ws";
     const userChannel = `/user/${id}/notifications`;
+    const client = new Client({
+        brokerURL,
+        onConnect: () => {
+            client.subscribe(userChannel, message => {
+                const quote = JSON.parse(message.body);
+                setIsConnected(true);
+                console.log(`Mensaje Recibido: ${quote}`)
+            });
+            client.onStompError = function (frame) {
+                console.log('Ocurrio un error: ' + frame.headers['message']);
+                console.log('Detalles del error: ' + frame.body);
+            };
+        },
+    });
 
     useEffect(() => {
-        socket.on("connect", () => {
-            setConection(socket.connected);
-            console.log(`Estado de conexion con websocket ${socket.connected ? "Establecida" : "No establecida"}`);
-        });
-
-        socket.on(userChannel, (data: Notification) => {
-            setNotifications([...notifications, data]);
-            console.log(data);
-        });
-
+        client.activate();
         return () => {
-            socket.disconnect();
+            client.deactivate();
         }
     }, []);
+
     return (
         <Container>
-            <Box>
-                <Box>
-                    <Typography variant="body2">
-                        {`Estado de conexion con websocket: `}
-                    </Typography>
-                    <Typography variant="body2" color={ conection ? "info" : "success"}>
-                        {`${conection ? "Establecida" : "No establecida"}`}
-                    </Typography>
-                </Box>
-
-                <Typography variant="body2" color="warning">
-                    {`Estado de reconexion: ${socket.active ? "Reconectando..." : ""}`}
-                </Typography>
-            </Box>
-            <List>
-                {
-                    notifications.length === 0
-                        ? notifications.map(({ id, isRead, text, user }) => (
-                            <ListItem>
-                                <Typography variant="body1" color="info">
-                                    {id}
-                                </Typography>
-                                <Typography variant="body1" color="info">
-                                    {isRead}
-                                </Typography>
-                                <Typography variant="body1" color="info">
-                                    {text}
-                                </Typography>
-                                <Typography variant="body1" color="info">
-                                    {JSON.stringify(user)}
-                                </Typography>
-                            </ListItem>
-                        ))
-                        : (
-                            <ListItem>
-                                SIN NOTIFICACIONES
-                            </ListItem>
-                        )
-                }
-            </List>
+            <Typography variant="body2" color="warning">
+                {`Estado de reconexion: ${isConnected ? "Reconectando..." : "Conectado"}`}
+            </Typography>
         </Container>
     );
 }
